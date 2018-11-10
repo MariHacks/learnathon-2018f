20dd36835afd9eb4cc86dbde6b4734de5b7c0905
marihacks <35971426+marihacks@users.noreply.github.com>
2018-11-10 16:07:09 -0500

    Create print-custom.board.py

---
+++ print-custom.board.py
@@ -0,0 +1,7 @@
+print("") # prints empty line
+print("Let's play tic tac toe!")
+print("")
+print(" {} | {} | {} ".format(board[0],board[1],board[2]))
+print(" {} | {} | {} ".format(board[3],board[4],board[5]))
+print(" {} | {} | {} ".format(board[6],board[7],board[8]))
+print("") # prints empty line
